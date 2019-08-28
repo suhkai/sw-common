694e6774f1dfa5d7d18c7a9470e6860169293e60
jacobbogers <jkfbogers@gmail.com>
2019-08-28 19:43:43 +0200

    cleaning , but not done yet

--- src/primer.ts
+++ src/primer.ts
@@ -4,11 +4,10 @@
 import SABootInfo from './bootstrap/components/SABootInfo';
 //import { create } from 'jss';
 import '@easyfonts/league-mono-typeface/ultralight.css';
-import '@easyfonts/saira-typeface/css/saira-condensed-600.css';
+import '@easyfonts/saira-typeface/css/saira-extra-condensed-600.css';
 import './globals.css';
 import './btstrp-general.css';
 import './btstrp-media-qry.css';
-
 
 const isHttpValidResponse = (response: Response) => {
     const { status, statusText } = response;
