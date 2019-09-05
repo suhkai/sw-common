c589320d377b35ca61826b7d9411033135860ddf
jacobbogers <jkfbogers@gmail.com>
2019-09-05 11:03:34 +0200

    found postcss.assets

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,38 @@
-//https://hackernoon.com/building-and-publishing-a-module-with-typescript-and-rollup-js-faa778c85396
-// ^this one looks good
 
-// following https://github.com/Microsoft/TypeScript-Babel-Starter#using-rollup
-
-// for css
-
-// https://lengstorf.com/learn-rollup-css/
 
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
 import typescript from 'rollup-plugin-typescript2';
-
-const extensions = ['.mjs', '.js', '.jsx', '.json', '.css', '.woff', '.woff2', '.ts', '.tsx'];
+import postcss from 'rollup-plugin-postcss';
+import simplevars from 'postcss-simple-vars';
+import nested from 'postcss-nested';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
+import url from "rollup-plugin-url"
 
 const plugins = [
+  url({
+    limit: 0, // inline files < 10k, copy files > 10k
+    include: ["node_modules/**/*.woff2"], // defaults to .svg, .png, .jpg and .gif files
+    emitFiles: true // defaults to true
+  }),  
+  postcss({
+    modules: true,
+    extract: true,
+    extensions: ['.css'],
+    plugins: [
+               simplevars(),
+               nested(),
+               cssnext({ warnForDuplicates: false, }),
+               cssnano(),
+    ],
+  }),
   nodeResolve({
-    module: true,
+    module: false,
     jsnext: true,
     main: true,
+    browser: true,
     preferBuiltins: false
   }),
   commonjs({
@@ -52,35 +65,13 @@ const plugins = [
     ]
   })
 ];
-/*
-options: {
-  presets: [
-    ["@babel/env", {
-      "useBuiltIns": "usage",
-      "corejs": {
-        version: 3,
-        proposals: true
-      }
-    }],
-    "@babel/preset-typescript",
-    // 'minify'
-  ],
-    plugins: [
-      "@babel/proposal-class-properties",
-      "@babel/proposal-object-rest-spread",
-      "@babel/plugin-transform-async-to-generator",
-      // 😠 😠😠😠
-      // below plugin helps resolve this safari issue((
-      // see link https://stackoverflow.com/questions/33878586/safari-babel-webpack-const-declarations-are-not-supported-in-strict-mode
-      "@babel/plugin-transform-block-scoping"
-    ]
-}*/
 
 export default {
   input: 'src/primer.ts',
   output: [{
     dir: 'dist',
-    format: 'umd'
+    format: 'iife'
   }],
+  sourceMap: 'inline',
   plugins
 }
--- src/primer.ts
+++ src/primer.ts
@@ -2,13 +2,11 @@
 
 //import createPlugins from './utils/jss-plugins';
 import SABootInfo from './bootstrap/components/SABootInfo';
-
 //css
-/*import '@easyfonts/saira-typeface/css/saira-extra-condensed-600.css';
+import '@easyfonts/saira-typeface/css/saira-extra-condensed-600.css';
 import './globals.css';
 import './btstrp-general.css';
 import './btstrp-media-qry.css';
-*/
 
 const isHttpValidResponse = (response: Response) => {
     const { status, statusText } = response;
