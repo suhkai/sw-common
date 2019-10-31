e7d6771b691f9c966e846fd1b2352d809827e5b8
jacobbogers <jkfbogers@gmail.com>
2019-10-31 19:44:07 +0100

    more tools

--- packages/validator/src/features/sanatizeFileName.js
+++ packages/validator/src/features/sanatizeFileName.js
@@ -1,1 +1,19 @@
-modules.exports = { V: createValidatorFactory() };
+function sanitizeFileName(name) {
+    return name.replace(/[\0?*+]/g, '_');
+}
+
+//https://stackoverflow.com/questions/1976007/what-characters-are-forbidden-in-windows-and-linux-directory-names
+// not really windows friendly path fragging, use path.parse and path.format and examine the 'root' element
+function isPlainPathFragment(name) {
+    // not starting with "/", "./", "../"
+    return (
+        name[0] !== '/' &&
+        !(name[0] === '.' && (name[1] === '/' || name[1] === '.')) &&
+        sanitizeFileName(name) === name
+    );
+}
+
+module.exports = {
+    sanitizeFileName,
+    isPlainPathFragment
+};
