c4989bb846624e8f0a3a489983b4fe10ce2d33d1
jacobbogers <jkfbogers@gmail.com>
2019-11-28 13:44:01 +0100

    researched sqllite3 and sequelize

--- packages/sequalize/index.js
+++ packages/sequalize/index.js
@@ -1,29 +1,69 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(
     {
-        storage: 'path/to/database.sqlite',
+        storage: 'path/to/chinook.db',
         dialect: 'sqlite'
     }
 );
 
+/**
+ * ER symbols
+ * --|--  one
+ * --<-- many
+ * --|-|-  one and only one
+ * --O|- zero or one
+ * -- |<-- one or many
+ * -- 0<-- zero or many
+ */
+
+function defineModels() {
+    class MediaTypes extends Model { };
+    // media_types
+    // MediaTypeId: INTEGER
+    // Name: NVARCHAR(120) -- no diff between VARCHAR and NVARCHAR for sqlite, the [N] means non ascii, aka utf8
+    MediaTypes.init({
+        MediaTypeId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+        Name: DataTypes.STRING(120)
+    }, { sequelize, modelName: 'media_types', timestamps: false, });
+
+    class Genres extends Model { };
+    // genres
+    // GenreId: INTEGER
+    // Name: NVARCHAR(120)
+    Genres.init({
+        GenreId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+        Name: DataTypes.STRING(120),
+        ['hello_world']: DataTypes.STRING,
+    }, { sequelize, modelName: 'genres', timestamps: false, });
+
+    class Tracks extends Model { };
+    // tracks
+    // TrackId: INTEGER
+    // Name: NVARCHAR(120)
+    Tracks.init({
+        TrackId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+        Name: DataTypes.STRING(200),
+
+    }, { sequelize, modelName: 'tracks', timestamps: false });
+
+    Tracks.hasOne(Tracks, { as: 'subTrack', foreignKey: 'subTrackId' });
+
+    Genres.hasMany(Tracks, { foreignKey: 'GenreId', targetKey: 'hello_world' });
+    MediaTypes.hasMany(Tracks, { foreignKey: 'MediaTypeId' });
+
+    return { MediaTypes, Genres };
+}
+
+
 sequelize.authenticate()
     .then(async () => {
         console.log('connection established');
-        class User extends Model { }
-        User.init({
-            username: DataTypes.STRING,
-            birthday: DataTypes.DATE
-        }, { sequelize, modelName: 'user' });
-
+        // define models i guess
+        defineModels();
         await sequelize.sync()
-        const jane = await User.create({
-                username: 'janedoe',
-                birthday: new Date(1980, 6, 20)
-            });
-        console.log(jane.toJSON());
-        await User.update({lastName: "Doe"}, { where: { lastName:null }});
-        console.log(User.get('lastName'));
-        await sequelize.close();
+
+
+        //await sequelize.close();
     })
     .catch(err => {
         console.log('error', err)
