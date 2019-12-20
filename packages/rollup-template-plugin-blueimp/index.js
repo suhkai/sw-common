ba5d35204a0b94fc86f86a2ff16eb9c715c4406d
jacobbogers <jkfbogers@gmail.com>
2019-12-20 15:54:38 +0100

    we have a nice object slicer going

--- packages/rollup-template-plugin-blueimp/index.js
+++ packages/rollup-template-plugin-blueimp/index.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const tmpl = require('blueimp-tmpl');
-
-
+const parse5 = require('parse5');
+const getTokens = require('../validator/getTokens');
+const objectSlice = require('../validator/objectSlice');
 const {
     V
 } = require('../validator');
@@ -24,10 +25,25 @@ const template = `
 </body>
 </html>`;
 
-async function generate(){
+async function generate(bundles, outputOptions){
    return tmpl(template, {title:'Rollup Sample App', base_url:'http://my-demo-app.com'});
 };
 
-generate().then(data=>console.log(data));
+generate().then(text=>{
+    
+    // fucking beautyfull, works:
+    const selector = getTokens('/childNodes/[nodeName=html]/childNodes/[nodeName=head]/childNodes/[nodeName=#comment]/');
+    const data = parse5.parse(text);
+    const result = objectSlice(data, selector);
+    console.log(result);
+    /* result ->
+      [ 'favicon injection',
+        'css asset injection',
+        'js asset injection'
+      ]
+    */  
+
+});
 
 // ok i have a good design now for this
+
