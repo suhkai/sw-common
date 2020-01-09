a3666f5fb05673bb8a27a3748e1d37f323e75fca
jacobbogers <jkfbogers@gmail.com>
2020-01-09 12:53:24 +0100

    sibling stuff walking from a sidebranch  works too

--- packages/async-iterator/stack.js
+++ packages/async-iterator/stack.js
@@ -7,7 +7,12 @@ const data = {
             child3: {
                 name: 'hier3'
             }
-        }
+        },
+        child2sibble: [
+            {
+                hello:'world'
+            }
+        ]
     }
 };
 
@@ -24,13 +29,22 @@ function init(){
     const p1 = createParent(data);
     const p2 = createParent(data.child, p1);
     const p3 = createParent(data.child.child2, p2);
+    const p3s = createParent(data.child.child2sibble[0], p2);
     const p4 = createParent(data.child.child2.child3, p3);
 
-    console.log(p4());
-    console.log(p4(1));
-    console.log(p4(2));
-    console.log(p4(3));
-    console.log(p4(4));
+    const log = p => console.log(JSON.stringify(p));
+    //
+    log(p4());
+    log(p4(1));
+    log(p4(2));
+    log(p4(3));
+    log(p4(4));
+    //
+    console.log('sibble');
+    log(p3s());
+    log(p3s(1));
+    log(p3s(2));
+    log(p3s(3));
 }
 
 init();
