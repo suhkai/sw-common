1753edaf824fd218bf50692f238b4109b4a3d272
jacobbogers <jkfbogers@gmail.com>
2020-02-07 17:46:46 +0100

    if you are locked into parse5ast then xpath could navigate much better on the ast

---
+++ packages/parse5-experiments/index.js
@@ -0,0 +1,27 @@
+const html = `
+<html>
+<head>
+<title>hello world</title>
+</head>
+<body>
+<!--     some comment value    -->
+<div>event some randome text <span>can</span> be used as anchor*
+</div>
+<body>
+</html>`;
+
+const { parse, parseFragment, serialize } = require('parse5');
+const { stringify } = require('flatted');
+const jxpath = require('@mangos/jxpath');
+
+const ast = parse(html);
+
+// /[nodeName=h]/childNodes/[nodeName=html]/childNodes/[nodeName=head]/childNodes/[nodeName=title]/childNodes/value
+console.log(jxpath('[nodeName=#document]/childNodes/[nodeName=html]/childNodes/[nodeName=body]/childNodes/[nodeName=div]/childNodes/[nodeName=span]/childNodes/[value=can]/..', ast));
+
+console.log(stringify(ast));
+/* something like 
+
+html/head/comment/
+
+*/
