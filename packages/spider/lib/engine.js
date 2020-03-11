67b364781d6ae4ada3141c24f87f98cc6a50c073
jacobbogers <jkfbogers@gmail.com>
2020-03-11 22:12:11 +0100

    forgot to add

--- packages/spider/lib/engine.js
+++ packages/spider/lib/engine.js
@@ -1,6 +1,24 @@
 'use strict';
 
-module.exports = function fromTask(queue){ // linked list?
-    // if ()
-                 
-};
+const { filter } = require('./utils/functionals');
+module.exports = function engine(queue, handlers, storage) { // linked list?
+    const selectBusy = filter(q => q.state !== 'waiting');
+    const selectFree = filter(q => q.state === 'waiting');
+    
+    let todo;
+    out:
+    do {
+        todo = selectFree(queue);
+        const { value: data, done } = todo.next();
+        if (done) { // no todo
+            if (selectBusy(queue).next().done)
+                break out; // nothing to do
+            }
+        }
+        throttle()
+        //
+        //
+
+
+
+    };
