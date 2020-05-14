34fa502b535d16cdb9eee5658ba738672653c92b
jacobbogers <jkfbogers@gmail.com>
2020-05-14 15:08:20 +0200

    whitespace absorber + unit tests done

---
+++ packages/css-parser/lib/white-space.js
@@ -0,0 +1,22 @@
+const { WS } = require('./tokens')
+const { isWS } = require('./checks-and-definitions')
+module.exports = function (iterator) {
+    const step = iterator.peek();
+    let prev = step.value;
+    iterator.next();
+    const start = { loc: { col: prev.col, row: prev.row }, o: prev.o }
+    let end;
+    while (!step.done) {
+        const _1 = step.value;
+        if (!isWS(_1.d)){
+            end = { loc: { col: prev.col, row: prev.row }, o: prev.o };
+            break;
+        }
+        prev = _1;
+        iterator.next();
+    }
+    if (!end) { // we hit the end of the stream
+        end = { loc: { col: prev.col, row: prev.row }, o: prev.o };
+    }
+    return { id: WS, s: start, e:end };
+};
--- packages/css-parser/test/createIterator.test.js
+++ packages/css-parser/test/createIterator.test.js
@@ -3,6 +3,8 @@ const chai = require('chai');
 const { expect } = chai;
 const createIterator = require('../lib/iterator');
 const absorbComments = require('../lib/comments');
+const absorbWhiteSpace = require('../lib/white-space');
+const { isWS } = require('../lib/checks-and-definitions')
 
 describe('createIterator', () => {
     it('test columns, row, follows css preprocessing rules', () => {
@@ -38,22 +40,61 @@ describe('createIterator', () => {
             { d: 't', col: 6, row: 6, o: 27 },
             { d: 's', col: 7, row: 6, o: 28 },
             { d: '\n', col: 8, row: 6, o: 29 }
-          ])
+        ])
     });
-    it('absorbCommenttest',()=>{
+    it('absorbCommenttest', () => {
         const data = 'some text\n\r\n/* some comment */';
         const iter = createIterator(data);
         const step = iter.next();
         //ff
-        while(step.value.d !== '/'){
+        while (step.value.d !== '/') {
             iter.next();
         }
         const _1 = step.value;
-        iter.next()
+        iter.next();
         const _2 = step.value;
-        const result = absorbComments(_1,_2,iter);
-        iter.next()
-        console.log(result, step.done)
+        const result = absorbComments(_1, _2, iter);
+        iter.next();
+        expect(step.done).to.be.true;
+        const text = data.slice(result.s.o, result.e.o + 1);
+        expect(text).to.equal('/* some comment */');
+        expect(result).to.deep.equal({
+            id: 2,
+            s: { loc: { col: 1, row: 3 }, o: 12 },
+            e: { loc: { col: 18, row: 3 }, o: 29 }
+        });
     })
-    
+    it('absorbWhiteSpace', () => {
+        const data = ' some text\n\r\n/* some comment */';
+        const iter = createIterator(data);
+        const step = iter.next();
+        while (!isWS(step.value.d)) {
+            iter.next();
+        }
+        const ws1 = absorbWhiteSpace(iter);
+        while (!isWS(step.value.d)) {
+            iter.next();
+        }
+        const ws2 = absorbWhiteSpace(iter);
+        while (!isWS(step.value.d)) {
+            iter.next();
+        }
+        const ws3 = absorbWhiteSpace(iter);
+        expect(ws1).to.deep.equal({
+            id: 3,
+            s: { loc: { col: 1, row: 1 }, o: 0 },
+            e: { loc: { col: 1, row: 1 }, o: 0 }
+        });
+        expect(ws2).to.deep.equal({
+            id: 3,
+            s: { loc: { col: 6, row: 1 }, o: 5 },
+            e: { loc: { col: 6, row: 1 }, o: 5 }
+        });
+        expect(ws3).to.deep.equal({
+            id: 3,
+            s: { loc: { col: 11, row: 1 }, o: 10 },
+            e: { loc: { col: 1, row: 2 }, o: 11 }
+        });
+    })
+
 });
