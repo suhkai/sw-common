f0fb3477df2f8d28d6c2ebc214867f8aeb7ab1fe
jacobbogers <jkfbogers@gmail.com>
2020-07-27 20:14:38 +0200

    got good idea for css parser showoff

--- packages/css-lexer/index.js
+++ packages/css-lexer/index.js
@@ -1,6 +1,6 @@
 const CachedStringIterator = require('./lib/lexer/cachedStringiterator');
 const CachedIterator = require('./lib/lexer/cachedIterator');
-const tokens = require('./lib/lexer/tokenizer');
+const tokens = require('./lib/lexer/tokens');
 const tokenizer = require('./lib/lexer/tokenizer')
 const atrule = require('./lib/parser/atrule');
 const component = require('./lib/parser/component');
@@ -9,12 +9,10 @@ const rule = require('./lib/parser/rule');
 const simpleBlock = require('./lib/parser/simple-block')
 
 module.exports = {
-    lexer: {
-        StringIterator: CachedStringIterator,
-        CachedIterator,
-        tokenMap: tokens,
-        tokenizer
-    },
+    Preprocessor: CachedStringIterator,
+    CachedIterator,
+    tokenMap: tokens,
+    tokenizer,
     recognizers: {
         atrule,
         component,
