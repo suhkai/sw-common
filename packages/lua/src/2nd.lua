b879dcfea09b244e994638187a107b493620d3a0
jacobbogers <jkfbogers@gmail.com>
2020-09-11 20:24:56 +0200

    3rd script for result procssing scaffolded, only left is processing the result of the secundary execution, and final processin (stats, whatever)

--- packages/lua/src/2nd.lua
+++ packages/lua/src/2nd.lua
@@ -118,7 +118,8 @@ if jobdescr == false and jinbody ~= false then
     -- reschedule
     sched = redis.call("zadd", KEYS[1], "CH", nextTs, jobId);
     return "next";
-end;
+  end;
+end;  
 
 --3. we have a job-description and a lingering record
 if jobdescr ~= false and jinbody ~= false then
@@ -133,13 +134,14 @@ if jobdescr ~= false and jinbody ~= false then
     else
       -- reschedule
       redis.call("zadd", KEYS[1], "CH", nextTs, jobId);
+      redis.call("set", jin, cjson.encode(jinBody)); -- updated "execCount"
       return 'next'
     end
   else  
     -- remove entry, previous jobinstance did not finish running
     -- skip it
     reddis.call("zrem", KEYS[1], jobId);
-    return "next";
+    return "next:prev job instance not finished, state:"..lastState;
   end
 end 
 
@@ -160,36 +162,213 @@ end
 -- local candidate = redis.call("zrangebyscore", KEYS[1], "-inf", ARGV[1], "")
 
 function jobPostExecution() -- use JobId and JobResult (as json value)
-
   -- find the jobinstance record
-    -- if it doesnt exist , exit with error
+  -- if it doesnt exist , exit with error
   -- if it exist, remove the jobinst from the jobexecute sorted setx
   -- make sure the jobinst is in your "state" (execution) otherwise report error, and clean up all (excpet jobinst)
   -- if the executionCount is lower then yours discard result, and report this error , clean out entry in jobexec
   -- if executionCount is higher or equal then yours, set it to your execCount, clean out entry in jobexec add state to history and insert into jobResult sorted set
-  -- 
+  -- place key in 
 
-
-
-  do -- scope 
-    local jobdescrRaw = redis.call("get",jobId);
-    if jobdescrRaw ~= false then
-       jobdescr = cjson.decode(jobdescrRaw);
-       local delta = jobdescr.info.interval or jobdescr.info.oneshot;
-       nextTs = ts + math.floor(delta/3);
-    else
-       nextTs = ARGV[1] + 1000;  
-    end;  
-    --2. job execution instance
-    jin = jobId.."Instance"
+  local jobId = KEYS[1] -- from the service
+  local jobExecSet = KEYS[2]; -- the exec "queue"
+  local jobResultSet = KEYS[3]; -- the result "queue"
+  local JobResult = false;
+  local jin = jobId.."Instance";
+  local jinBody = false;
+  local lastState = false;
+  local lastTs = false;
+  local currentTime;
+  -- fetch the instance redord
+  do 
+    local time = redis.call('time');
+    currentTime = time[1] + math.trunc(time[2]/1000);
     local jsonstr = redis.call("get", jin);
     if jsonstr ~= false then
-        jinbody = cjson.decode(jsonstr);
-        local lastIdx = #jinbody.history;
-        lastState = jinbody.history[lastIdx].state;
-        lastTs = jinbody.history[lastIdx].time;
+      jinBody = cjson.decode(jsonstr);
+      local lastIdx = #jinBody.history;
+      lastState = jinBody.history[lastIdx].state;
+      lastTs = jinBody.history[lastIdx].time;
     end;
- end 
+    jobResult = cjons.decode(ARGV[1]);
+  end;
 
+  -- if th    jinBody doesnt exist abort with an error
+  if jinBody == false then
+     redis.call("ZREM", jobExecSet, jobId);
+     return "error:jobinstance not found";   
+  end;
+  
+  -- job must be in the correct state
+  if lastState ~= "executing" then
+     redis.call("ZREM", jobExecSet, jobId);
+     return "error:jobinstance has state: "..lastState;
+  end
+  
+  -- save the jobresult,and progress state  
+  jinBody.execResult = jobResult;
+  table.insert(jinBody.history, { state = 'executed', time = currentTime });
+  redis.call('set', jin , cjson.encode(jinBody));
+  --- remove the entry in jobExec
+  redis.call("zrem", jobExecSet, jobId);
+  --  add entry in jobResult
+  redis.call("zadd", jobResultSet, "CH", currentTime, jobId);
+  
+  return 'ok:advanced';
+end;
 
+function jobProcessResults() 
+
+  --[[
+     use JobId and JobResult (as json value)
+     get jobid based on time
+     get jobinstance
+        if jobinstance not exist, then abort with error, cleanout entry in jobResult "queue"
+     get last state
+     get last time
+     get execResult from jinbody
+     get onSuccess from jobDescr
+     get onError from jobDescr
+     if (state === "processingresult") 
+         if processresult count > 2 then 
+            make top state "executed"
+            goto "process as normal"
+         else 
+            advance time /3 , ++processresult and yield
+     end   
+
+     if (state ~= "executed") then -- something wrong
+       delete entry in jobResult queue
+       yield error
+     end
+     -- 
+     -- is there and execResult ?
+     local reply = false
+     if (execResult ~= false) then
+       local reply = false
+       if (onError ~= false and execResult.error) then -- process error
+         reply = cjson.encode({ execInfo=onError, errorResult = execResult.error });
+       end
+       if (onSuccess ~= false) then
+         reply = cjson.encode({ execInfo=onSuccess, successResult = execResult.data });
+       end 
+     end;  
+     remove entry in jobResultSet queue
+     if reply == false then 
+       -- just skip to next without doing nothing
+       jinBody.history add "resultprocessed" and  ts
+       
+       add entry in jobFinalSet queue
+       save jinBody
+       return "next:delivered jobfinal:"..jobId
+     else
+       jinBody.history add "processingresult", ts
+       save jinBody
+       return reply; 
+      end
+    end;    
+     
+      
+
+     if onResult ~= false and execResult ~= falset then
+        nothing to do here clean out jobResult queue
+        move to queue jobDone
+        add state entry
+        save jobInst
+     end
+
+]]--
+
+-- inputs
+local jobId = KEYS[1]
+local jobResultSet = KEYS[2]; -- queue 
+local ts = ARGV[1]
+
+-- lookup keys
+local jin = jobId.."Instance";
+-- optoions
+local execResult = false;
+local onSuccess = false;
+local onFailere = false;
+local lastState = false;
+local nextTs = false;
+local jinBody = false;
+local currentTime;
+
+do --local scoping
+  local time = redis.call('time');
+  currentTime = time[1] + math.trunc(time[2]/1000);
+
+  local jsonstr = redis.call("get", jin);
+  if jsonstr ~= false then
+    jinBody = cjson.decode(jsonstr);
+   
+    local lastIdx = #jinBody.history;
+    lastState = jinBody.history[lastIdx].state;
+    lastTs = jinBody.history[lastIdx].time;
+   
+    local delta = jinBody.jobDescr.info.interval or jinBody.jobDescr.info.oneshot;
+    nextTs = ts + math.floor(delta/5);
+   
+    if type(jinBody.jobDescr.onSuccess) == 'table' then
+      onSuccess = jinBody.jobDescr.onSuccess;
+      onFailure = jinBody.jobDescr.onFailure;
+    end;
+    execResult = jinBody.execResult;
+  end; -- if
+end -- do
+
+-- checks
+-- 1.no jinbody 
+if jinBody == false then
+   redis.call("zrem", jobResultSet, jobId);
+   return "error:jobinstance not found, "..jobId;
 end
+
+--2. is the state "processingresult"
+if lastState == "processingresult" then
+  jinBody.processingresult = (jinBody.processingresult or 0) + 1;
+  if jinBody.processingresult > 2 then -- existing post processig crashed? so retry
+     table.insert(jinBody.history, { state = "executed", time = ts });
+     lastState = "executed";
+     lastTs = currentTime;
+  else
+     -- reschedule in future
+     redis.call("zadd", jobResultSet, "CH", currentTime, jobId );
+     redis.call("set", jin, cjson.encode(jinBody));
+     return "next:existing execution found";
+  end;
+end;     
+
+if (lastState ~="executed") then --wrong state abort
+  redis.call("zrem", jobResultSet, jobId);
+  return "error:jobInstance in wrong state, "..lastState;
+end  
+
+
+local handler = false;
+if (onSuccess ~= false and execResult ~= nil and execResult.error == nil) then
+  handler = onSuccess;
+elseif (onFailure ~= false and execResult ~= nil and execResult.error ~= nil) then
+  handler = onFailure;
+elseif onSuccess ~= nil then
+  handler = onSuccess;
+end;    
+
+-- 1. did previous step NOT select a handler?, skip
+if (handler == false) then
+  redis.call("zrem", jobResultSet, jobId);
+  table.insert(jinBody.histroy, { state = "resultprocessed", time = currentTime });
+  redis.call("zadd", jobFinalSet, "CH", currentTime, JobId);
+  redis.call("set", cjson.encode(jinBody));
+  return 'next:skipped to final, '..jinId;
+end
+
+--2. handler exist optionally pass a payload
+ 
+redis.call("zadd", jobResultSet, "CH", nextTs, JobId);
+table.insert(jinBody.histroy, { state = "resultprocessing", time = currentTime });
+redis.call("set", cjson.encode(jinBody));
+return cjson.encode({ response = execResult, handler=handler });
+end -- function
+
