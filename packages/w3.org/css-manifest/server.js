501ca9d1997522cd778d00aa2bad7d184ccba2c4
jacobbogers <jkfbogers@gmail.com>
2020-10-01 20:56:38 +0200

    manifest.json stuff

--- packages/w3.org/css-manifest/server.js
+++ packages/w3.org/css-manifest/server.js
@@ -1,48 +1,29 @@
 'use strict';
-const http = require("http");
-const url = require("url");
-const path = require("path");
-const fs = require("fs");
 
-const mimeExtMaps = {
-      ".html": "text/html",
-      ".jpeg": "image/jpeg",
-      ".jpg": "image/jpeg",
-      ".png": "image/png",
-      ".svg": "image/svg+xml",
-      ".json": "application/json",
-      ".js": "text/javascript",
-      ".css": "text/css",
-      ".webmanifest":"application/manifest+json"
-};
- 
-const port = process.argv[2] || 8888;
+const express = require('express');
+const mimes = require('mime-types');
+const app = express();
 
-//-- createMiddleware, root
-// use (pattern, handlerId, handler)
-// pattern -> handlerId (can be several 1:n)
-// handler-id -> handler
-
-function createMiddleWare( onError, notFound){
-  return {
-    use(pathOrExp, handle){
-
-    },
-    remove(pathOrExp, handle){
-
+app.use(
+  express.static('public',{
+    dotfiles:'allow',
+    etag: true,
+    extensions: ['html','htm'],
+    fallthrough: false,
+    immutable: false,
+    index:"index.html",
+    redirect: true,
+    setHeaders(res,path, stat){ // handle headers here for each file
+      if (path.endsWith('manifest.json')){
+        const mime = mimes.lookup('.webmanifest');
+        res.setHeader('Content-Type',mime);
+      }
     }
   }
-}
+));
 
+const port = process.argv[2] || 8888;
 
-const server = http.createServer((request, response) => {
-  const uparts = url.parse(request.url);
-  console.log(request.url);
-  console.log(JSON.stringify(uparts));
-  response.writeHead(200, {'Content-Type':'text/html'});
-  response.end();
-});
- 
 function getPort(){
   const p1 = parseInt(process.env.port, 10);
   if (Number.isFinite(p1)&& p1 > 1024 && p1 < 32000){
@@ -51,7 +32,7 @@ function getPort(){
   return 8080;
 }
 
-server.listen(getPort(),function (){
+app.listen(getPort(),function (){
   console.log(`Listening on: ${this._connectionKey}`)
 });
 
