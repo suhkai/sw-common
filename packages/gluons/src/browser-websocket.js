2a3f54cb0776ce41ed7c8dd6ad8f2b9a42de8d54
jacobbogers <jkfbogers@gmail.com>
2020-10-14 13:53:58 +0200

    "reactive" programming for tcp data pipelines

--- packages/gluons/src/browser-websocket.js
+++ packages/gluons/src/browser-websocket.js
@@ -1,69 +1,112 @@
 'use strict'
 
 export function createWebSocket(url, options) {
-    return (hooks) => {
-        const encoder = new TextEncoder();
-        const decoder = new TextDecoder();
-        return {
-            create() {
-                let ws;
-                try {
-                    ws = new WebSocket(url, options);
-                }
-                catch (err) {
-                    return [err]
-                }
-                ws.binaryType = 'arraybuffer';
-                ws.onclose = e => {
-                    hooks.onClose(e.reason, e.wasClean);
-                }
-                ws.onopen = () => {
-                    hooks.onOpen();
-                };
-                ws.onerror = () => hooks.onError();
-                ws.onmessage = ({ data }) => hooks.onData(data);
-                return [undefined, ws];
-            },
-            close(ws, reason, done) {
-                let text
-                if (typeof reason === 'string' && reason.length) {
-                    let rawBytes = encoder.encode(reason);
-                    if (rawBytes.length >= 62) {
-                        rawBytes = rawBytes.splice(0, 60);
-                    }
-                    text = decoder.decode(rawBytes);
-                }
-                try {
-                    if (text) {
-                        ws.close(1000, text);
-                    }
-                    else {
-                        ws.close(1000);
-                    }
-                }
-                catch (err) {
-                    done && done(err);
-                    return;
-                }
-                done && done();
-            },
-            /*reconnectStrategy(ws, info, transmissionsStats){ // return time delay, if not fixed delay of 500ms
-    
-            },*/
-            send(ws, data, done) {  // metadata has information on remote host, port, other things that are use full depending on wire transfer technology
-                if (ws.readyState !== WebSocket.OPEN) {
-                    done && done(`websocket in in correct state ${ws.readyState}`);
-                    return;
-                }
-                ws.send(data);
-                done && done(); // don't defer it for this socket
-            },
-            isOpen(ws){
-                return ws && ws.readyState === WebSocket.OPEN;
-            }
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
+    let ws;
+    let cnt = 0;
+
+    function closeSafe() {
+        if (ws && ws.readyState === WebSocket.OPEN) {
+            return false;
         }
+        if (ws) {
+            try {
+                ws.onclose = null;
+                ws.close();
+            }
+            catch (e) { /**/ }
+        }
+        ws = undefined;
+        return true;
+
     }
+
+    return pipe => ({
+        create() {
+            if (!closeSafe()) {
+                return false;
+            }
+            try {
+                let pUrl = url;
+                let pOptions = options;
+                if (typeof url === 'function') {
+                    [pUrl, pOptions] = url(cnt++);
+                }
+                ws = new WebSocket(pUrl, pOptions);
+            }
+            catch (err) {
+                return [err]
+            }
+            ws.binaryType = 'arraybuffer';
+            ws.onclose = e => {
+                pipe.onClose(e.reason, e.wasClean);
+            }
+            ws.onopen = () => {
+                pipe.onReady();
+            };
+            ws.onerror = () => {
+                pipe.onError();
+            }
+            ws.onmessage = ({ data }) => {
+                pipe.onData(data);
+            }
+            return [undefined, ws];
+        },
+        close(reason, done) {
+            if (!ws) {
+                return true;
+            }
+            let text
+            if (typeof reason === 'string' && reason.length) {
+                let rawBytes = encoder.encode(reason);
+                if (rawBytes.length >= 62) {
+                    rawBytes = rawBytes.splice(0, 60);
+                }
+                text = decoder.decode(rawBytes);
+            }
+            let err;
+            try {
+                if (text) {
+                    ws.close(1000, text);
+                }
+                else {
+                    ws.close(1000);
+                }
+            }
+            catch (e) {
+                err = e;
+            }
+            finally {
+                done && 
+                ( 
+                    (err && done(err))
+                    || 
+                    done() 
+                );
+            }
+        },
+        send(data, done) {  // metadata has information on remote host, port, other things that are use full depending on wire transfer technology
+            if (!ws || ws.readyState !== WebSocket.OPEN) {
+                done(`socket not open: readyState=${ws.readyState}`);
+                return;
+            }
+            try {
+                ws.send(data);
+            }
+            catch (err) {
+                done(err);
+                return;
+            }
+            done();
+        },
+        isReady() {
+            return ws && ws.readyState === WebSocket.OPEN;
+        }
+    });
 }
+
+
 
 
 
