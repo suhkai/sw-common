464bd00ba8480976c12b54bb1312f10447943727
jacobbogers <jkfbogers@gmail.com>
2020-10-28 19:53:02 +0100

    write handlers from proc files

---
+++ packages/proc/metrics.js
@@ -0,0 +1,20 @@
+const handlers = [
+  '/proc/cpuinfo',
+  '/prof/self/io',
+  '/proc/self/map_files',
+  '/proc/self/pagemap',
+  '/proc/self/smaps',
+  '/proc/self/statm',
+  '/proc/self/status',
+  '/proc/self/buddyinfo',
+  '/proc/loadavg',
+  '/proc/meminfo',
+  '/proc/self/net/dev',
+  '/proc/uptime',
+  '/proc/self/stat',
+  '/proc/self/limits'
+
+
+];
+
+module.exports = handlers;
