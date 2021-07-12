f36595bb0c174b3e7dc3353cb3fb0d87e7b9aa16
jacobbogers <jkfbogers@gmail.com>
2021-07-12 22:30:42 +0200

    bitfinex scan and some gradle/kotlin exercises

--- packages/bitfinex/index.js
+++ packages/bitfinex/index.js
@@ -7,250 +7,115 @@ node bfx_test_book.js BTCUSD
 const WS = require('ws')
 const fs = require('fs')
 const CRC = require('crc-32')
-const { resolve } = require('path');
-
-const pair = 'BTCUSD';
+const {
+  resolve
+} = require('path');
+const { Console } = require('console');
 
 const host = 'wss://api.bitfinex.com/ws/2';
 
-const logfile = resolve('logs/ws-book-aggr.log')
-
-const BOOK = {};
-BOOK.bids = {}
-BOOK.asks = {}
-BOOK.psnap = {}
-    
-
-console.log(pair, host)
-
 let ws
 
-let seq = null
+/* ... do things for a while ... */
 
-const subs =  {};
+function formatDate(){
+  const d = Date.now();
+  const millis = d % 1000;
+  const pref = new Date(d).toISOString().slice(0,-5);
+  return `${pref}|${millis}`;
+}
 
-function connect () {
-  ws = new WS(host, { /* rejectUnauthorized: false */ })
+
+function connect() {
+  ws = new WS(host, {
+    /* rejectUnauthorized: false */
+  })
   
-  console.log('/event/open registered');
+  
+  ws.on('error', err => {
+    console.log('/event/error')
+    console.error(`ERR: error event received: ${String(err)}`);
+  });
+
+  
   ws.on('open', () => {
-    console.log('/event/open')
-    BOOK.mcnt = 0
-    ws.send(JSON.stringify({ event: 'conf', flags: 65536 + 131072 }))
-    ws.send(JSON.stringify({ event: 'subscribe', channel: 'book', pair: pair, prec: 'P0', len: 100 }))
+    ws.send(JSON.stringify({
+      event: 'conf',
+      flags: 65536 + 131072
+    }))
+    ws.send( JSON.stringify({
+      event: 'subscribe',
+      channel: 'book',
+      prec: 'R0',
+      symbol: 'tBTCUSD',
+      len: 250
+    }));
   })
 
-  console.log('/event/close registered');
+  
   ws.on('close', (err, code) => {
-    seq = null
     console.log('/event/close', err, code);
+    console.log(`closed at ${new Date()}`);
+    seq = null
   })
 
-  let cnt = 1;
-  let info; // =  {}
-  let conf; // = {};
- 
-  const channels = {};
   let seq;
   ws.on('message', (data) => {
-    msg = JSON.parse(data)
-    console.log(`/event/message/${cnt++}:\n${data}\n`);
-    if (msg?.event === 'info'){
-        info = msg;
-        return;
+    const dateStr = formatDate()
+    let msg = JSON.parse(data)
+    if (msg?.event === 'info') {
+      info = msg;
+      return;
     }
-    if (msg?.event === 'conf'){
-        conf = msg;
-        return;
+    if (msg?.event === 'conf') {
+      conf = msg;
+      return;
     }
-    if (msg?.event === 'subscribed'){
-      console.log(subs);
-        subs[msg.symbol] = msg;
-        channels[msg.chanId] = msg;
-        return;
+    if (msg?.event === 'subscribed') {
+      return;
     }
     if (!Array.isArray(msg)) {
-        console.error(`There is an error: ${data}`);
-        ws.close();
-        return;
+      console.error(`ERR: There is an error: ${data}`);
+      console.error(`ERR: Error happend at ${new Date()}`);
+      ws.close();
+      return;
     }
-    //const get channel id
     const channelId = msg[0];
-    const sub = channels[channelId];
     const payload = msg[1];
     const sequence = msg[1] === 'cs' ? msg[3] : msg[2];
-    if (sequence === 1 && seq === undefined){
+    if (sequence === 1 && seq === undefined) {
+      console.log('seq,channelId,datetime,orderid,price,amount');
       seq = 1;
-    }
-    else if (sequence - seq !== 1){
-        console.log(`OUT OF SEQUENCE:${seq}-${sequence}, diff=${sequence-seq}`);
+    } else if (sequence - seq !== 1) {
+      console.log(`ERR: OUT OF SEQUENCE:${seq}-${sequence}, diff=${sequence - seq}`);
     }
     seq = sequence;
-
-    if (msg[1] === 'hb'){
-      if (!Number.isInteger(msg[2])){
-          console.log(`'hb' doesnt have integer as payload ${data}`);
-          ws.close();
-          return;
+    if (msg[1] === 'hb') {
+      if (!Number.isInteger(msg[2])) {
+        console.error(`ERR: 'hb' doesnt have integer as payload ${data}`);
+        ws.close();
+        return;
       }
       seq = msg[2];
-      console.log('hb response:'+seq);
+      console.log(`${seq},${channelId},${dateStr},hb`);   // heart beat??
       return;
     }
-    if (msg[1] === 'cs'){
-      console.log('check sum received');
+    if (msg[1] === 'cs') {
+      console.log(`${seq},${channelId},${dateStr},cs,${msg[2]}`);
       return;
     }
-  
+
 
     // receiving snapshot of orderbook
-    if (Array.isArray(payload) && payload.length === 200){
-       console.log(`I think this is a snapshot: ${msg[1].length}`);
-       return;
+    if (Array.isArray(payload) && payload.length > 4) {
+      for (const order of payload) {
+        const [orderid, price, amount] = order;
+        console.log(`${seq},${channelId},${dateStr},${orderid},${price},${amount}`);
+      }
+      return;
     }
-   
-
-
-
-
+    const [orderid, price, amount] = payload;
+    console.log(`${seq},${channelId},${dateStr},${orderid},${price},${amount}`);
   });
-    /*
-    if (msg.event) return
-    if (msg[1] === 'hb') {
-      seq = +msg[2]
-      return
-    } else if (msg[1] === 'cs') {
-      seq = +msg[3]
-
-      const checksum = msg[2]
-      const csdata = []
-      const bids_keys = BOOK.psnap['bids']
-      const asks_keys = BOOK.psnap['asks']
-
-      for (let i = 0; i < 25; i++) {
-        if (bids_keys[i]) {
-          const price = bids_keys[i]
-          const pp = BOOK.bids[price]
-          csdata.push(pp.price, pp.amount)
-        }
-        if (asks_keys[i]) {
-          const price = asks_keys[i]
-          const pp = BOOK.asks[price]
-          csdata.push(pp.price, -pp.amount)
-        }
-      }
-
-      const cs_str = csdata.join(':')
-      const cs_calc = CRC.str(cs_str)
-
-      fs.appendFileSync(logfile, '[' + moment().format('YYYY-MM-DDTHH:mm:ss.SSS') + '] ' + pair + ' | ' + JSON.stringify(['cs_string=' + cs_str, 'cs_calc=' + cs_calc, 'server_checksum=' + checksum]) + '\n')
-      if (cs_calc !== checksum) {
-        console.error('CHECKSUM_FAILED')
-        process.exit(-1)
-      }
-      return
-    }
-
-    // normal?
-    fs.appendFileSync(logfile, '[' + moment().format('YYYY-MM-DDTHH:mm:ss.SSS') + '] ' + pair + ' | ' + JSON.stringify(msg) + '\n')
-
-    if (BOOK.mcnt === 0) {
-      // process snapshot
-      _.each(msg[1], function (pp) {
-        pp = { price: pp[0], cnt: pp[1], amount: pp[2] }
-        const side = pp.amount >= 0 ? 'bids' : 'asks'
-        pp.amount = Math.abs(pp.amount)
-        if (BOOK[side][pp.price]) {
-          fs.appendFileSync(logfile, '[' + moment().format() + '] ' + pair + ' | ' + JSON.stringify(pp) + ' BOOK snap existing bid override\n')
-        }
-        BOOK[side][pp.price] = pp
-      })
-    } else {
-      // incremental update
-      const cseq = +msg[2]
-      msg = msg[1]
-      if (!seq) {
-        seq = cseq - 1
-      }
-      if (cseq - seq !== 1) {
-        console.error('OUT OF SEQUENCE', seq, cseq)
-        process.exit()
-      }
-
-      seq = cseq
-
-      let pp = { price: msg[0], cnt: msg[1], amount: msg[2] }
-
-      if (!pp.cnt) {
-        let found = true
-
-        if (pp.amount > 0) {
-          if (BOOK['bids'][pp.price]) {
-            delete BOOK['bids'][pp.price]
-          } else {
-            found = false
-          }
-        } else if (pp.amount < 0) {
-          if (BOOK['asks'][pp.price]) {
-            delete BOOK['asks'][pp.price]
-          } else {
-            found = false
-          }
-        }
-
-        if (!found) {
-          fs.appendFileSync(logfile, '[' + moment().format() + '] ' + pair + ' | ' + JSON.stringify(pp) + ' BOOK delete fail side not found\n')
-        }
-      } else {
-        let side = pp.amount >= 0 ? 'bids' : 'asks'
-        pp.amount = Math.abs(pp.amount)
-        BOOK[side][pp.price] = pp
-      }
-    }
-
-    _.each(['bids', 'asks'], function (side) {
-      let sbook = BOOK[side]
-      let bprices = Object.keys(sbook)
-
-      let prices = bprices.sort(function (a, b) {
-        if (side === 'bids') {
-          return +a >= +b ? -1 : 1
-        } else {
-          return +a <= +b ? -1 : 1
-        }
-      })
-
-      BOOK.psnap[side] = prices
-    })
-
-    BOOK.mcnt++
-    checkCross(msg)
-  })*/
 }
-/*
-setInterval(function () {
-  if (connected) return
-  connect()
-}, 3500)
-
-function checkCross (msg) {
-  let bid = BOOK.psnap.bids[0]
-  let ask = BOOK.psnap.asks[0]
-  if (bid >= ask) {
-    let lm = [moment.utc().format(), 'bid(' + bid + ')>=ask(' + ask + ')']
-    fs.appendFileSync(logfile, lm.join('/') + '\n')
-    console.log(lm.join('/'))
-  }
-}
-
-function saveBook () {
-  const now = moment.utc().format('YYYYMMDDHHmmss')
-  fs.writeFileSync(__dirname + "/logs/tmp-ws-book-aggr-" + pair + '-' + now + '.log', JSON.stringify({ bids: BOOK.bids, asks: BOOK.asks}))
-}
-
-setInterval(function () {
-  saveBook()
-}, 30000)
-*/
-
 connect();
