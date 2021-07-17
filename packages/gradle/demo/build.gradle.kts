0333527db18d712aa61e9bbeb1018706f4c719e5
jacobbogers <jkfbogers@gmail.com>
2021-07-17 21:30:46 +0200

    kotlin for gradle

--- packages/gradle/demo/build.gradle.kts
+++ packages/gradle/demo/build.gradle.kts
@@ -1,3 +1,84 @@
+import java.util.Date;
+import java.io.File;
+import java.util.Map;
+import java.io.BufferedReader;
+
+fun readDictionary(file: File): Map<String, *> = file.inputStream().use {
+    TODO("Read a mapping of strings to arbitrary elements.")
+}
+
+val intsFile = File("ints.dictionary")
+
+tasks.register("type-checks3") {
+    doLast {
+        // We saved a map with `Int`s into that file
+        //  --> not giving the warning below as expected
+        // Warning: Unchecked cast: `Map<String, *>` to `Map<String, Int>`
+        
+        val intsDictionary: Map<String, Int> = readDictionary(intsFile) as Map<String, Int>
+      
+    }
+}
+
+inline fun <reified A, reified B> Pair<*, *>.asPairOf(): Pair<A, B>? {
+    if (first !is A || second !is B) return null
+    return first as A to second as B
+}
+
+tasks.register("type-checks2") {
+    doLast {
+        val somePair: Pair<Any, Any> = "items" to listOf(1, 2, 3)
+
+        val stringToSomething = somePair.asPairOf<String, Any>()
+        val stringToInt = somePair.asPairOf<String, Int>()
+        val stringToList = somePair.asPairOf<String, List<*>>()
+        val stringToStringList = somePair.asPairOf<String, List<String>>()
+
+        println("stringToSomething = " + stringToSomething)
+        println("stringToInt = " + stringToInt)
+        println("stringToList = " + stringToList)
+        println("stringToStringList = " + stringToStringList)
+        // below will cause classcast exception
+        //println(stringToStringList.second.forEach() {it.length})
+        //TODO("Read a mapping of strings to arbitrary elements.")
+    }
+}
+
+tasks.register("type-checks") {
+    doLast {
+        val x: Any = IntArray(4);
+        /*if (x is String){
+            println("length="+x.length)
+        }
+        else if (x is Date){
+            println("its a date")
+        }*/
+        when(x) {
+            is Int -> print(x+1)
+            is String -> print(x.length+1);
+            is Array<*> -> println("its an array")
+            is IntArray -> print(x.sum());
+        }
+        val d = Date(90)
+        println(d);
+        val y: Any? = null;
+        val y2: String? = y as? String;
+        println(y2 === null);
+        (x as IntArray).forEach { k -> println(k) }
+        (x as IntArray).forEach { println(it) }
+        var l = List(5, { it*2+1 });
+        println(l);
+        if (l is List<*>){
+            l.forEach { println(it) };
+        }
+        val list = List<String>(5, { "${it}" } );
+        println(list::class);
+        // -> class java.util.ArrayList
+
+       
+
+    }
+}
 
 tasks.register("strings") {
     doLast {
