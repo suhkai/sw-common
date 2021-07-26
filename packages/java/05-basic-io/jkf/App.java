3ff7d5f1ec59f34cabf29bb3da7f0a8b9229a74d
jacobbogers <jkfbogers@gmail.com>
2021-07-27 00:31:34 +0200

    java-turtle io

--- packages/java/05-basic-io/jkf/App.java
+++ packages/java/05-basic-io/jkf/App.java
@@ -14,6 +14,14 @@ import java.io.BufferedReader;
 //
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.util.Scanner;
+import java.util.Locale;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+
 
 import jkf.CloseSafe;
 
@@ -27,14 +35,14 @@ public class App {
 
         BufferedInputStream in = null;
         BufferedOutputStream out = null;
-        
+
         try {
             in = new BufferedInputStream(new FileInputStream(iname));
             out = new BufferedOutputStream(new FileOutputStream(oname));
             int c;
             for (;;) {
                 c = in.read();
-                System.out.println((char)c);
+                System.out.println((char) c);
                 if (c < 0)
                     break;
                 out.write(c);
@@ -75,7 +83,6 @@ public class App {
             closeSafe.close(out);
         }
     }
-
 
     void copyCharacters(String iname, String oname) {
 
@@ -118,20 +125,120 @@ public class App {
                     break;
                 out.println(l);
             }
-        } 
-        catch(FileNotFoundException  fnfe){
+        } catch (FileNotFoundException fnfe) {
 
-        }
-        catch(IOException  ioe){
+        } catch (IOException ioe) {
 
-        }
-        finally {
+        } finally {
             System.out.println(String.format("input file: %s", in.getClass().getName()));
             System.out.println(String.format("output file: %s", out.getClass().getName()));
             closeSafe.close(in);
             closeSafe.close(out);
         }
+
+    }
+
+    public void scanXan(String iname) {
+
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader(iname)));
+            while (s.hasNext()) {
+                System.out.println(s.next());
+            }
+
+        } catch (FileNotFoundException fnfe) {
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+    }
+
+    public void scanSum(String iname) {
+        Scanner s = null;
+        double sum = 0;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader(iname)));
+            s.useLocale(Locale.US);
+
+            while (s.hasNext()) {
+                if (s.hasNextDouble()) {
+                    sum += s.nextDouble();
+                } else {
+                    s.next();
+                }
+            }
+        } catch (FileNotFoundException ffne) {
+            System.out.format("%s%n", ffne);
+        } finally {
+            closeSafe.close(s);
+        }
+        System.out.println(sum);
+    }
+
+    // Console
+    public void readConsole() {
+
+        Console c = System.console();
+        if (c == null) {
+            System.err.println("No console/tty");
+            return;
+        }
+        String login = c.readLine("Enter your login: ");
+        char[] oldPassword = c.readPassword("Enter your old password: ");
+        System.out.format("user: %s%n", login);
+        System.out.format("pwd: %s%n", new String(oldPassword));
+    }
+
+    public void readWriteDataStream(String iname) {
+        // some data
+        int[] ints = { 1, 3, 34, 2342, 42435 };
+        double[] doubles = { 30.23, 234.23, -890.01 };
+        String text = "✋"; //"✋🌍";
         
+        var cwd = System.getProperty("user.dir");
+        var filePath = Paths.get(cwd, iname);        
+        
+        try (FileOutputStream fos = new FileOutputStream(filePath.toString()); DataOutputStream dos = new DataOutputStream(fos)) {
+            for (int i : ints) {
+                //dos.writeInt(i);
+            }
+            for (double d : doubles) {
+                //dos.writeDouble(d);
+            }
+            dos.writeUTF(text);
+
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("fnfe error happened");
+            System.out.println(fnfe);
+        } catch (IOException ioe) {
+            System.out.println("ioe error happened");
+            System.out.println(ioe);
+        }
+
+        try (FileInputStream fis = new FileInputStream(filePath.toString()); DataInputStream dis = new DataInputStream(fis)) {
+            for (int i : ints) {
+                //var result = dis.readInt();
+                //System.out.println(result);
+            }
+            for (double d : doubles) {
+                //var result = dis.readDouble();
+                //System.out.println(result);
+            }
+            var string = dis.readUTF();
+            System.out.format("%s%n", string);
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("fnfe error happened");
+            System.out.println(fnfe);
+        } catch (IOException ioe) {
+            System.out.println("ioe error happened");
+            System.out.println(ioe);
+        }
+
+
     }
 
     public App() {
@@ -141,10 +248,14 @@ public class App {
     public static void main(String... argv) {
         var app = new App();
         var src = "xanadu.txt";
-       
-        app.copyBytes(src, "outgoing.txt");
-        app.copyCharacters(src,"outgoing_char.txt");
-        app.copyLines(src, "outgoingLines.txt");
-        app.copyBytesBuffered(src, "outgoingBuffered.txt" );
+
+        // app.copyBytes(src, "outgoing.txt");
+        // app.copyCharacters(src, "outgoing_char.txt");
+        // app.copyLines(src, "outgoingLines.txt");
+        // app.copyBytesBuffered(src, "outgoingBuffered.txt");
+        // app.scanXan(src);
+        // app.scanSum("usnumbers.txt");
+        // app.readConsole();
+        app.readWriteDataStream("datastream.bin");
     }
 }
--- packages/java/05-basic-io/jkf/CloseSafe.java
+++ packages/java/05-basic-io/jkf/CloseSafe.java
@@ -1,46 +1,14 @@
 package jkf;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.Closeable;
 
 public class CloseSafe {
-    void close(InputStream is){
-        if (is == null){
+    void close(Closeable s){
+        if (s == null){
             return;
         }
         try {
-            is.close();
-        }
-        catch(Throwable e){}
-    }
-    void close(OutputStream os){
-        if (os == null){
-            return;
-        }
-        try {
-            os.close();
-        }
-        catch(Throwable e){}
-    }
-
-    void close(Reader r){
-        if (r == null){
-            return;
-        }
-        try {
-            r.close();
-        }
-        catch(Throwable e){}
-    }
-    
-    void close(Writer w){
-        if (w == null){
-            return;
-        }
-        try {
-            w.close();
+            s.close();
         }
         catch(Throwable e){}
     }
