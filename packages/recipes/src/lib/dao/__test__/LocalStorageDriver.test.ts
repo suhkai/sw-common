125e58f29cda6d55d6c4f87fb530a8e9e9a412cd
jacobbogers <jkfbogers@gmail.com>
2021-09-06 23:48:06 +0200

    typecasting also works))

--- packages/recipes/src/lib/dao/__test__/LocalStorageDriver.test.ts
+++ packages/recipes/src/lib/dao/__test__/LocalStorageDriver.test.ts
@@ -5,6 +5,8 @@ import '@testing-library/jest-dom'
 import 'jest-localstorage-mock';
 
 import { LocalStorageDriver } from '../LocalStorageDriver';
+
+const mock = jest.fn((a:number) => ''+a);
  
 describe('LocalStorageDriver', () => {
      it('initalize', () => {
@@ -13,5 +15,8 @@ describe('LocalStorageDriver', () => {
          console.log(lsd.hasStorage());
          lsd.saveAll();
          console.log(globalThis.localStorage.length);
+         console.log((globalThis.localStorage.setItem as jest.Mock<void,[a:string, b:string]>).mock.calls);
+         (globalThis.localStorage.setItem as any).mockClear();
+         console.log((globalThis.localStorage.setItem as any).mock.calls);
      });
  })
