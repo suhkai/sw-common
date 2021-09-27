e8530cc9adcb0f1b82e05e7281cd766bd4105131
jacobbogers <jkfbogers@gmail.com>
2021-09-27 10:14:39 +0200

    cleaned up svelteconfig

--- packages/recipes/svelte.config.js
+++ packages/recipes/svelte.config.js
@@ -2,7 +2,6 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
-
 	kit: {
 		adapter: adapter(),
 		amp: false,
@@ -29,7 +28,9 @@ const config = {
 		trailingSlash: 'never'
 	},
 	preprocess: preprocess({
-		replace: [['process.env.NODE_ENV', JSON.stringify(process.env.NODE_ENV)]],
+		replace: [
+			['process.env.NODE_ENV', JSON.stringify(process.env.NODE_ENV)]
+		],
 		typescript: {
 			tsconfigFile: './tsconfig.app.json'
 		}
