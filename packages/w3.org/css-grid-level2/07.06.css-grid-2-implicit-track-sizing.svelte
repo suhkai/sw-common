44fc4e380ca07d1c3cbee171de6609d217e79ec7
jacobbogers <jkfbogers@gmail.com>
2022-01-02 14:39:03 +0100

    chapter 7 done of grid-css-2

---
+++ packages/w3.org/css-grid-level2/07.06.css-grid-2-implicit-track-sizing.svelte
@@ -0,0 +1,20 @@
+<style>
+    #grid {
+      display: grid;
+      grid-template-columns: 40px;
+      grid-auto-columns: max-content;
+      grid-template-rows: 40px;
+      grid-auto-rows: auto;
+    }
+    #A { grid-column: 1; grid-row: 1; border: 4px solid blue;}
+    #B { grid-column: 2; grid-row: 1; border: 4px solid orange;}
+    #C { grid-column: 1; grid-row: 2; border: 4px solid red;}
+    #D { grid-column: 2; grid-row: 2; border: 4px solid cyan;}
+  </style>
+  
+  <div id="grid">
+    <div id="A">A</div>
+    <div id="B">B</div>
+    <div id="C">C</div>
+    <div id="D">D</div>
+  </div>
---
+++ packages/w3.org/css-grid-level2/07.07-css-grid-2-automatic-placement.svelte
@@ -0,0 +1,87 @@
+<form>
+    <label for="firstname">First name:</label>
+    <input type="text" id="firstname" name="firstname" />
+    <label for="lastname">Last name:</label>
+    <input type="text" id="lastname" name="lastname" />
+    <label for="address">Address:</label>
+    <input type="text" id="address" name="address" />
+    <label for="address2">Address 2:</label>
+    <input type="text" id="address2" name="address2" />
+    <label for="city">City:</label>
+    <input type="text" id="city" name="city" />
+    <label for="state">State:</label>
+    <select type="text" id="state" name="state">
+        <option value="WA">Washington</option>
+    </select>
+    <label for="zip">Zip:</label>
+    <input type="text" id="zip" name="zip" />
+
+   
+
+    <div id="buttons">
+        <button id="cancel">Cancel</button>
+        <button id="back">Back</button>
+        <button id="next">Next</button>
+    </div>
+
+    <label class="department-block" for="department">Department:</label>
+    <select class="department-block" id="department" name="department" multiple>
+        <option value="finance">Finance</option>
+        <option value="humanresources">Human Resources</option>
+        <option value="marketing">Marketing</option>
+    </select>
+</form>
+
+<style type="text/css">
+    * {
+        box-sizing: border-box;
+    }
+    form {
+        display: grid;
+        /* Define three columns, all content-sized,
+         and name the corresponding lines. */
+        grid-template-columns: [labels] auto [controls] auto [oversized] auto;
+        grid-auto-flow: row dense;
+        column-gap: 1ch;
+        align-items: center;
+    }
+    form > label {
+        /* Place all labels in the "labels" column and
+         automatically find the next available row. */
+        grid-column: labels;
+        grid-row: auto;
+    }
+    form > input,
+    form > select {
+        /* Place all controls in the "controls" column and
+         automatically find the next available row. */
+        grid-column: controls;
+        grid-row: auto;
+    }
+
+    .department-block {
+        /* Auto place this item in the "oversized" column
+         in the first row where an area that spans three rows
+         won’t overlap other explicitly placed items or areas
+         or any items automatically placed prior to this area. */
+        grid-column: oversized;
+        /*grid-row: span 3;*/
+    }
+
+    #department {
+        grid-row: span 3;
+        align-self: stretch;
+        border: 4px solid red;
+    }
+
+    /* Place all the buttons of the form
+       in the explicitly defined grid area. */
+    #buttons {
+        grid-row: auto;
+
+        /* Ensure the button area spans the entire grid element
+         in the inline axis. */
+        grid-column: 1 / -1;
+        text-align: end;
+    }
+</style>
