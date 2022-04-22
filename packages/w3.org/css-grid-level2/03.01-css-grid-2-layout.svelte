380e62004ec2c84eb120daad8bf8667e818476fc
jacobbogers <jkfbogers@gmail.com>
2022-04-22 13:36:39 +0200

    w3.org/css-align-3 everything works till §3.3

--- packages/w3.org/css-grid-level2/03.01-css-grid-2-layout.svelte
+++ packages/w3.org/css-grid-level2/03.01-css-grid-2-layout.svelte
@@ -1,66 +1,68 @@
+<div id="grid-implicit-rows">
+	<div id="item1">item1</div>
+	<div id="item2">item2</div>
+	<div id="item3">item3</div>
+	<!-- there will be an empty cell in the grid because of only 3 items -->
+</div>
+
+<div id="grid-using-grid-lines">
+	<div id="item4">item4</div>
+	<div id="item5">item5</div>
+	<div id="item6">item6</div>
+	<!-- there will be an empty cell in the grid because of only 3 items -->
+</div>
+
 <style>
-	
-	
-	* {
+	:global(*),
+	:global(*::before),
+	:global(*::after) {
 		box-sizing: border-box;
 	}
-	
-	#grid {
-  	display: grid;
-  	grid-template-columns: 150px 1fr;
-  	grid-template-rows: 50px 1fr 50px;
+
+	/* EXAMPLE 6: https://www.w3.org/TR/css-grid-2/#grid-line-concept*/
+	#grid-implicit-rows {
+		display: grid;
+		grid-template-columns: 150px 1fr;
+		grid-template-rows: 50px 1fr 50px;
+		border: 4px solid orange;
 	}
-	
-	/*#item1 {
+
+	#item1 {
 		grid-column: 2;
-    grid-row-start: 1; grid-row-end: 4; 
+        grid-row-start: 1; 
+		grid-row-end: 4;  /* implicit rows will be created here */
+		background: rgba(255,0, 255, 0.5);
+		border: 4px dashed black;
 	}
-	
-	#grid {
-  	display: grid;
-  	grid-template-columns: 150px [item1-start] 1fr [item1-end];
-  	grid-template-rows: [item1-start] 50px 1fr 50px [item1-end];
-	}*/
-	
-	#grid  {
-		border:2px solid orange;
-  	display: grid;
-  	grid-template-areas: ". a"
-    	                   "b a"
-      	                 ". a";
-  	grid-template-columns: 150px 1fr;
-  	grid-template-rows: 50px 1fr 50px;
-		overflow-x: auto;
-}
 
-#item1 { 
-	border: 2px solid brown;
-	background: rgba(192,0,0,0.2); 
-	grid-area: a;
-}
-	
-#item2 { 
-	background: rgba(0, 192,0,0.2);
-	grid-area: b;
-	border: 2px solid blue;
-}
-	
-#item3 { 
-	border: 2px solid red;
-	background: rgba(0,0,192,0.2);
-	grid-area: b;
-}
+	#item2 {
+		background: lightcyan;
+	}
 
-/* Align items 2 and 3 at different points in the grid area "b".  */
-/* By default, grid items are stretched to fit their grid area    */
-/* and these items would layer one over the other. */
-#item2 { align-self: start; justify-self: start; }
-#item3 { align-self: end; justify-self: end;  }
-	
+	#item3 {
+		background: lightyellow;
+	}
+
+	/* USING GRID LINES */
+
+	#grid-using-grid-lines {
+		display: grid;
+		margin-top: 1rem;
+		border: 4px pink solid;
+		grid-template-columns: 150px [item1-start] 1fr [item1-end];
+		grid-template-rows: [item1-start] 50px 1fr 50px [item1-end];
+	}
+
+	#item4 {
+		background: lightgreen;
+		grid-column: item1-start / item1-end;
+		grid-row: item1-start / item1-end;
+	}
+	#item5 {
+		background: lightgoldenrodyellow;
+	}
+
+	#item6 {
+		background: lightsalmon;
+	}
 </style>
-
-<div id="grid">
-	<div id="item1">item1</div>
-	<div id="item2">item2</div>	
-	<div id="item3">item3</div>	
-</div>
---
+++ packages/w3.org/css-grid-level2/03.03-css-grid-2-layout.svelte
@@ -0,0 +1,55 @@
+<div id="grid-areas">
+	<div id="item1">item1</div>
+	<div id="item2">item2</div>
+	<div id="item3">item3</div>
+	<!-- there will be an empty cell in the grid because of only 3 items -->
+</div>
+
+<style>
+	:global(*),
+	:global(*::before),
+	:global(*::after) {
+		box-sizing: border-box;
+	}
+
+	/* EXAMPLE 6: https://www.w3.org/TR/css-grid-2/#grid-line-concept*/
+	#grid-areas {
+		display: grid;
+		grid-template-areas:
+			". a"
+			"b a"
+			". a";
+		grid-template-columns: 150px 1fr;
+		grid-template-rows: 50px 1fr 50px;
+	}
+
+	#item1 {
+		grid-area: a;
+		background: rgba(255, 0, 255, 0.5);
+		border: 4px dashed black;
+	}
+
+	#item2 {
+		grid-area: b;
+		background: lightcyan;
+		align-self: start;
+	}
+
+	#item3 {
+		grid-area: b;
+		background: lightyellow;
+	}
+
+	/* 
+		align-items/align-self: <block-axis>/<cross-axis> alignment
+		justify-items/justify-self: <inline-axis>/<main-axis> alignment
+
+		justify-content: <inline-axis>/<main-axis>
+		align-content: <block-axis>/<cross-axis>
+	*/
+
+	#item2 { align-self: start; justify-self: start; border: 4px solid rgba(255,0,0,0.5); }
+	#item3 { justify-self: end; align-self: end; border: 5px solid rgba(0,255,0,0); }
+
+	
+</style>
