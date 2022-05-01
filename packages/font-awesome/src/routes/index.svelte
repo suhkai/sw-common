080c74d299585c0d58f7c9d3452522c6565ca12d
jacobbogers <jkfbogers@gmail.com>
2022-05-01 20:08:27 +0200

    could analyse the fontawesome all.css, (free tier) now to use fontforge to delete all glyphs

--- packages/font-awesome/src/routes/index.svelte
+++ packages/font-awesome/src/routes/index.svelte
@@ -1,4 +1,50 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import css from 'css';
+	import jxpath from '@mangos/jxpath';
+	import cssText from '$lib/css';
+
+	let contentClassMap = new Map<string, Set<string>>();
+
+	onMount(() => {
+		var obj = css.parse(cssText);
+		const iteratorRules = jxpath('/stylesheet/rules/[type=rule]/', obj);
+		for (const rule of iteratorRules) {
+			// find decleration type "content"
+
+			const contentDeclaration = Array.from(
+				jxpath('/declarations/[property=content]/[type=declaration]/value', rule)
+			);
+			if (contentDeclaration.length === 1) {
+				const content: string = contentDeclaration[0] as string;
+				const selectors: string[] = Array.from(jxpath('/selectors', rule));
+				for (const selector of selectors) {
+					const existingSelectors = contentClassMap.get(content) || new Set();
+					
+					if (/::/.test(selector)){
+						existingSelectors.add(selector.slice(1).replace(/::before/, ''));
+						
+					}
+					else if (/:/.test(selector)){
+						existingSelectors.add('fa-brands '+selector.slice(1).replace(/:before/, ''));
+					}
+					else {
+						continue;
+					}
+					contentClassMap.set(content, existingSelectors);
+				}
+			} else {
+				if (contentDeclaration.length) {
+					throw new Error(
+						`nr contentDeclaration > 0 && !==1, [${
+							contentDeclaration.length
+						}], for rule:${rule.selectors.join(' ')}`
+					);
+				}
+			}
+		}
+		contentClassMap = contentClassMap;
+	});
 </script>
 
 <ul>
@@ -18,7 +64,13 @@
 </ul>
 
 <ul>
-	<li class:fa={true} class:fa-align-center={true} class:fa-2x={true} class:fa-rotate-by={true} class:fa-flip={true}/>
+	<li
+		class:fa={true}
+		class:fa-align-center={true}
+		class:fa-2x={true}
+		class:fa-rotate-by={true}
+		class:fa-flip={true}
+	/>
 	<li>fa-align-center(flip)</li>
 	<li>(flip)</li>
 </ul>
@@ -48,34 +100,48 @@
 </ul>
 
 <ul style="--fa-animation-direction: reverse">
-	<li class:fa={true} class:fa-anchor-circle-check={true} class:fa-2x={true} class:fa-spin={true}  />
+	<li class:fa={true} class:fa-anchor-circle-check={true} class:fa-2x={true} class:fa-spin={true} />
 	<li>fa-anchor-circle-check</li>
 	<li>(spin + spin-reverse)</li>
 </ul>
 
-<ul >
-	<li class:fa={true} class:fa-anchor-circle-exclamation={true} class:fa-2x={true} class:fa-spin={true} />
+<ul>
+	<li
+		class:fa={true}
+		class:fa-anchor-circle-exclamation={true}
+		class:fa-2x={true}
+		class:fa-spin={true}
+	/>
 	<li>fa-anchor-circle-exclamation</li>
 	<li>(spin-pulse)</li>
 </ul>
 
-<ul >
+<ul>
 	<li class:fa={true} class:fa-anchor-circle-xmark={true} class:fa-2x={true} />
 	<li>fa-anchor-circle-exclamation</li>
 	<li>(spin-pulse)</li>
 </ul>
 
-
+<div class="gridbox">
+	{#each [...contentClassMap.entries()] as contentEntry, i}
+		<ul>
+			{#each [...contentEntry[1].keys()] as classEntry, j}
+				{#if j === 0}
+					<li class="fa fa-2x {classEntry}" />
+				{/if}
+				<li>{classEntry}</li>
+			{/each}
+		</ul>
+	{/each}
+</div>
 
 <style>
 	:host {
 		font-family: 'Font Awesome 6 Free';
-		
 	}
 
 	:root {
 		--not-active-fa-rotate-angle: 30deg;
-		
 	}
 
 	:global(ul) > :global(li):nth-child(1) {
@@ -87,11 +153,22 @@
 	:global(ul) > :global(li):nth-child(3),
 	:global(ul) > :global(li):nth-child(4),
 	:global(ul) > :global(li):nth-child(5),
-	:global(ul) > :global(li):nth-child(6)
-	{
+	:global(ul) > :global(li):nth-child(6) {
 		display: table-row;
 		font-family: 'Sans Serif';
 		font-size: 12px;
 		list-style-type: none;
+	}
+	.gridbox {
+		display:grid;
+		width: 100%;
+/*		gap: 10px;
+    	grid-auto-flow: column;
+		grid-auto-columns: max-content;*/
+
+		grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
+  gap: 10px;
+  grid-auto-flow: dense;
+
 	}
 </style>
--- packages/user-credentials-svelte/src/routes/index.svelte
+++ packages/user-credentials-svelte/src/routes/index.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import EmailInput from '$lib/EmailInput.svelte';
 	import PasswordInput from '$lib/PasswordInput.svelte';
+	import { a } from '$lib/something';
+	console.log(a);
 </script>
 
 <div>
