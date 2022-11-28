412074a513525db5e11a9eac546eba883aa4f48c
Jacob Bogers <jkfbogers@gmail.com>
2022-11-28 20:52:42 +0100

    outline the design for matching namespaces

--- packages/femtologger/src/utils/nsSelected.ts
+++ packages/femtologger/src/utils/nsSelected.ts
@@ -3,5 +3,15 @@ export default function isNSSelected(ns: string | null | undefined, pattern: str
         return false;
     }
     // for now only return true (all ns are enabled)
+    // 1. wildcard '*' everything is allowed
+    // 2. comma seperated list (can contain wildcards)
+    // 2. prefix '-' means NOT equal to specified patter
+
+    // scan for cvs list (no commas means 1 item)
+    //   list: "-*, hello-word" -> means  disable everything (?) and allow for hello world, means dont show anything 
+    //  data* -> compiles to /^data.*$/
+    //  hello Mr *, how are you -> compiles to /^hello Mr .*, how are you$/
+    //  -hello* -> compiles to /^hello.*$/ but should not match
+
     return true;
 }
