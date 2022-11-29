1a9dc28cf0711199b870aed6a263ef8e53500b49
Jacob Bogers <jkfbogers@gmail.com>
2022-11-29 23:07:02 +0100

    used acorn parser, jxpath, escodegen to alter commonjs files before save

--- packages/femtologger/scripts/build.js
+++ packages/femtologger/scripts/build.js
@@ -5,8 +5,14 @@ import { mkdirSync, readFileSync, rmdirSync } from 'fs';
 import { writeFile } from 'fs/promises';
 import { join, relative, dirname } from 'path';
 import ts from 'typescript';
+import { parse } from 'acorn';
+import jxpath from '@mangos/jxpath';
+import { generate } from 'escodegen';
+
 
 const DIR = './dist';
+const DIR_COMMONJS = './dist/commonjs';
+const DIR_ESM = './dist/esm'
 
 // Delete and recreate the output directory.
 try {
@@ -14,8 +20,8 @@ try {
 } catch (error) {
   if (error.code !== 'ENOENT') throw error;
 }
-mkdirSync(DIR);
-
+mkdirSync(DIR_COMMONJS, { recursive: true});
+mkdirSync(DIR_ESM, { recursive: true});
 // Read the TypeScript config file.
 const { config } = ts.readConfigFile('tsconfig.json', (fileName) =>
   readFileSync(fileName).toString(),
@@ -25,13 +31,14 @@ const sourceDir = join('src');
 const sourceFile = join('src', 'index.ts');
 
 // Build CommonJS module.
-compile([sourceFile], { module: ts.ModuleKind.CommonJS });
+compile([sourceFile], DIR_COMMONJS, { module: ts.ModuleKind.CommonJS, declaration: false });
 
 // Build an ES2015 module and type declarations.
 
-compile([sourceFile], {
+compile([sourceFile], DIR_ESM, {
   module: ts.ModuleKind.ES2020,
   declaration: true,
+  declarationDir: './types' // this becomes ./dist/types
 });
 
 /**
@@ -40,18 +47,18 @@ compile([sourceFile], {
  * @param {string[]} files
  * @param {ts.CompilerOptions} options
  */
-function compile(files, options) {
+function compile(files, targetDIR, options) {
   const compilerOptions = { ...config.compilerOptions, ...options };
   const host = ts.createCompilerHost(compilerOptions);
 
   host.writeFile = (fileName, contents) => {
     const isDts = fileName.endsWith('.d.ts');
     const relativeToSourceDir = relative(sourceDir, fileName);
-    const subDir = join(DIR, dirname(relativeToSourceDir));
+    const subDir = join(targetDIR, dirname(relativeToSourceDir));
 
     mkdirSync(subDir, { recursive: true});
+    let path = join(targetDIR, relativeToSourceDir);
 
-    let path = join(DIR, relativeToSourceDir);
     if (!isDts) {
       switch (compilerOptions.module) {
         case ts.ModuleKind.CommonJS: {
@@ -59,6 +66,45 @@ function compile(files, options) {
           // eslint-disable-next-line no-param-reassign
           contents += `module.exports = exports.default;\nmodule.exports.default = exports.default;\n`;
           // Use the .cjs file extension.
+          const astTree = parse(contents, {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            ranges: true,
+            locations: false
+          });
+          const selectedNodes = jxpath(
+            '/**/[type=CallExpression]/callee/[type=Identifier]/[name=require]/../arguments/[type=Literal]/[value=/\..?js$/]/',
+            astTree
+          ); 
+          // loop over all .js and change then
+          for (const node of selectedNodes){
+             node.value = node.value.replace(/\..?js/,'');
+             node.raw = node.raw.replace(/\..?js/,'');
+          }
+          contents = generate(astTree);
+          //console.log(Array.from(data));
+          
+          // with jxpath we wcould do like this
+          //  /**/[type=CallExpression]/callee/[type=Identifier]/[name=require]/../arguments/[type=Literal]/[value=something.js]
+
+          /*Node {
+            type: 'CallExpression',
+            start: 10,
+            end: 33,
+            callee: Node { type: 'Identifier', start: 10, end: 17, name: 'require' },
+            arguments: [
+              Node {
+                type: 'Literal',
+                start: 18,
+                end: 32,
+                value: 'something.js',
+                raw: "'something.js'"
+              }
+            ],
+            optional: false
+          }
+          */
+          
           path = path.replace(/\.js$/, '.cjs');
           break;
         }
