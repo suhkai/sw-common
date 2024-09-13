d774ee6f1dafc81b22c2d68a51456e6732512866
jacobbogers <jkfbogers@gmail.com>
2024-09-13 18:01:32 +0200

    sync, continue work later

---
+++ packages/ui-componentes/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import SVGPPlIcon from './assets/SVGPPlIcon';
+
+function ShipperSearchListBox() {
+  return false;
+}
+
+
+function App() {
+  return (
+    <>
+        <SVGPPlIcon style={{ width: '24px' }}/>
+        <ShipperSearchListBox />
+    </>
+  )
+}
+
+export default App
